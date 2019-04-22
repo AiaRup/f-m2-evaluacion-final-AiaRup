6b983dcafbdd7e30dfd640b57f9a4356715f30b9
AiaRup <aiale88@gmail.com>
2019-04-22 08:43:31 +0200

    api fetch working

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,52 @@
 'use strict';
 
-console.log('>> Ready :)');
+// Elements to work with
+const searchButton = document.querySelector('.form__button');
+const searchInput = document.querySelector('.form__input');
+const seriesList = document.querySelector('.series');
+
+// Variables to work with
+const defaultImage =
+  'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+const favorities = [];
+const apiUrl = 'http://api.tvmaze.com/search/shows?q=';
+
+// Functions
+const takeUserInput = () => {
+  const value = {
+    text: searchInput.value,
+    status: true,
+  };
+  // check if value is empty
+  if (!value.text) {
+    value.status = false;
+  } else {
+    // SHOW MESSAGE
+  }
+  return value;
+};
+
+const showSeries = ({ show }) => {
+  console.log('show', show);
+};
+const searchSeries = url => {
+  const userValue = takeUserInput();
+  // if user entered a search value search the api
+  if (userValue.status) {
+    console.log(`${url}${userValue.text}`);
+    fetch(url + userValue.text)
+      .then(response => response.json())
+      .then(data => {
+        console.log('data', data);
+        for (const series of data) {
+          showSeries(series);
+        }
+      });
+  }
+};
+const favoriteSeries = () => {};
+
+// Event listeners
+searchButton.addEventListener('click', () => {
+  searchSeries(apiUrl);
+});
